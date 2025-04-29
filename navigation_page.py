dbc5aa842625af1ff0b4b54c3f9cd6c1100a5e39
Song Jihun <sksmsvlxk@gmail.com>
2025-04-29 14:34:47 +0900

    Feat: 네비게이션에 로고 추가해봄.

--- navigation_page.py
+++ navigation_page.py
@@ -9,3 +9,17 @@ pages = {
 
 pg = st.navigation(pages)
 pg.run()
+
+st.markdown(
+    """
+        <style>
+            [data-testid="stSidebar"] {
+                background-image: url(https://cdn.discordapp.com/attachments/1349656172692766722/1366641943035252817/logo.png?ex=6811affe&is=68105e7e&hm=3ea461a0899ee462e47db5140a88eb66902576f6bc40b4609df33e8ffae69b64&);
+                background-repeat: no-repeat;
+                padding-top: 80px;
+                background-size: 335px;
+            }
+        </style>
+        """,
+    unsafe_allow_html=True,
+)
